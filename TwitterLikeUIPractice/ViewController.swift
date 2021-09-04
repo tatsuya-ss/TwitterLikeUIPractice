8b04f515e88e9c9326aa11ca0a1a9c6375e08c05
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-04 17:33:59 +0900

    tableViewの横スクロールまで実装

---
+++ TwitterLikeUIPractice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TwitterLikeUIPractice
+//
+//  Created by 坂本龍哉 on 2021/09/04.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
